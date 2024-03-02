04d7cc6c44a58da69f2258293607aedb1eedb3d9
Fachrytb <tubagus.fakhridhiya@gmail.com>
2024-03-02 17:05:44 +0700

    update main.js

--- main.js
+++ main.js
@@ -64,7 +64,7 @@ export async function tambahPembeli(nama, alamat, nohp) {
   }
 }
 
-export async function ubahPembeli(docId, nama, nohp) {
+export async function ubahPembeli(docId, nama, alamat, nohp) {
   await updateDoc(doc(db, "pembeli", docId), { 
     nama: nama,
     alamat: alamat,
